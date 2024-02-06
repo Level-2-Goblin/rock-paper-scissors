807a5e5cbfeeff2102137c630b17af7e672420ab
Level-2-Goblin <153963097+Level-2-Goblin@users.noreply.github.com>
2024-02-06 11:06:44 -0700

    Make playRound func that outputs result
    Made a playRound() func that takes the player
    selection and computer random selection and tells
    you who won the round. Also added prompt for
    player selection that turns user input into lower
    case.

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 function getComputerChoice(min, max) {
   let choice = Math.floor(Math.random() * (max - min + 1) + min);
   if (choice == 1) {
-    return "Rock";
+    return "rock";
   } else if (choice == 2) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Scissors";
+    return "scissors";
   }
 }
 
-const playerSelection = "rock";
+function playRound(playerSelection, computerSelection) {
+  let win = `You win! ${playerSelection} beats ${computerSelection}!`;
+  let lose = `You lose! ${computerSelection} beats ${playerSelection}!`;
+  let tie = `Tie round! You both chose ${playerSelection}!`;
+  if (playerSelection == "rock" && computerSelection == "rock") {
+    return tie;
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    return win;
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    return lose;
+  } else if (playerSelection == "paper" && computerSelection == "paper") {
+    return tie;
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    return lose;
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return win;
+  } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+    return tie;
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    return lose;
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return win;
+  }
+}
+
+const playerSelection = prompt(
+  "Choose rock, paper, or scissors: "
+).toLowerCase();
 const computerSelection = getComputerChoice(1, 3);
+console.log(playRound(playerSelection, computerSelection));
